35d026c4f53dd22cfa23043b4fc7e936cd6494f1
shlazov <serhii.hlazov@contractos.medallia.com>
2022-07-05 12:45:48 +0200

    db connections

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,31 +1,9 @@
 const graphql = require('graphql');
 
-const {
-    GraphQLObjectType,
-    GraphQLString,
-    GraphQLSchema,
-    GraphQLID,
-    GraphQLInt,
-    GraphQLList,
-} = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt, GraphQLList } = graphql;
 
-const movies = [
-    { id: '1', name: 'Pulp Fiction', genre: 'Crime', directorId: '1', },
-    { id: '2', name: '1964', genre: 'Sci-Fi', directorId: '2', },
-    { id: '3', name: 'V for vendetta', genre: 'Sci-Fi-Triller', directorId: '3', },
-    { id: '4', name: 'Snatch', genre: 'Crime-Comedy', directorId: '4', },
-    { id: '5', name: 'Reservoir Dogs', genre: 'Crime', directorId: '1', },
-    { id: '6', name: 'The Hateful Egg', genre: 'Crime', directorId: '1', },
-    { id: '7', name: 'Snatch', genre: 'Crime', directorId: '1', },
-    { id: '8', name: 'Look, Stock and Two Smocking Barrels', genre: 'Crime-Comedy', directorId: '4', },
-];
-
-const directors = [
-    { id: '1', name: 'Quentin Tarantino', age: 55 },
-    { id: '2', name: 'Michael Radford', age: 72 },
-    { id: '3', name: 'James McTregue', age: 51 },
-    { id: '4', name: 'Guy Ritchie', age: 50 },
-];
+const Movies = require('../models/movie');
+const Directors = require('../models/director');
 
 const MovieType = new GraphQLObjectType({
     name: 'Movie',
@@ -33,13 +11,14 @@ const MovieType = new GraphQLObjectType({
         id: { type: GraphQLID },
         name: { type: GraphQLString },
         genre: { type: GraphQLString },
-        directorId: {
+        director: {
             type: DirectorType,
-            resolve(parent) {
-                return directors.find(({ id }) => id == parent.id)
+            resolve(parent, args) {
+                // return directors.find(director => director.id === parent.id);
+                return Directors.findById(parent.directorId);
             }
-        },
-    })
+        }
+    }),
 });
 
 const DirectorType = new GraphQLObjectType({
@@ -51,10 +30,11 @@ const DirectorType = new GraphQLObjectType({
         movies: {
             type: new GraphQLList(MovieType),
             resolve(parent, args) {
-                return movies.filter(movie => movie.directorId === parent.id);
-            }
-        }
-    })
+                // return movies.filter(movie => movie.directorId === parent.id);
+                return Movies.find({ directorId: parent.id });
+            },
+        },
+    }),
 });
 
 const Query = new GraphQLObjectType({
@@ -62,37 +42,37 @@ const Query = new GraphQLObjectType({
     fields: {
         movie: {
             type: MovieType,
-            args: {
-                id: { type: GraphQLID },
-            },
+            args: { id: { type: GraphQLID } },
             resolve(parent, args) {
-                return movies.find(({ id }) => id === args.id)
+                // return movies.find(movie => movie.id === args.id);
+                return Movies.findById(args.id);
             },
         },
         director: {
             type: DirectorType,
-            args: {
-                id: { type: GraphQLID },
-            },
+            args: { id: { type: GraphQLID } },
             resolve(parent, args) {
-                return directors.find(({ id }) => id === args.id)
-            }
+                // return directors.find(director => director.id === args.id);
+                return Directors.findById(args.id);
+            },
         },
         movies: {
             type: new GraphQLList(MovieType),
             resolve(parent, args) {
-                return movies;
+                // return movies;
+                return Movies.find({});
             }
         },
         directors: {
             type: new GraphQLList(DirectorType),
             resolve(parent, args) {
-                return directors;
+                // return directors;
+                return Directors.find({});
             }
         }
-    },
+    }
 });
 
 module.exports = new GraphQLSchema({
-    query: Query
-})
+    query: Query,
+});
--- server/server/app.js
+++ server/server/app.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./../schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = 3005;
+
+main().catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect('mongodb+srv://admin:admin@cluster0.uotql.mongodb.net/graphql');
+}
 
 app.use('/graphql', graphqlHTTP({
     schema,
     graphiql: true
 }));
+
+const dbConnection = mongoose.connection;
+dbConnection.on('error', err => console.log(`DB connection: ${err}`));
+dbConnection.once('open', () => console.log(`Connected to DB`));
 
 app.listen(PORT, err => {
     err ? console.log(err) : console.log('Server started!');
